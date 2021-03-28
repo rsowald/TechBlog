471e2ea065e13ae8e00c1425aab9b6d303d633b9
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-28 00:07:45 -0700

    changed casing in requires

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
