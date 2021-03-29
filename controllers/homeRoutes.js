179de259529647c7071fe60036371240ab7b610b
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-29 11:41:20 -0700

    renamed routes for consistency and exported each route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -26,3 +26,5 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+module.exports = router;
