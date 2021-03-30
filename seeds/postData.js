9fdd92ef9e5a2b9c05bd8af8f1046f5f909cf053
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-30 15:09:02 -0700

    added date and user id to post seeds

--- seeds/postData.js
+++ seeds/postData.js
@@ -3,15 +3,21 @@ const { Post } = require('../models')
 const postData = [
     {
         "title": "Why MVC is so important",
-        "content": "It's all about separation of concerns! You have the Model layer for data, the View layer for design, and the Controller layer for application logic. It is important to have separate code so each part has a meaningful and intuitive role while still being able adapt to change."
+        "content": "It's all about separation of concerns! You have the Model layer for data, the View layer for design, and the Controller layer for application logic. It is important to have separate code so each part has a meaningful and intuitive role while still being able adapt to change.",
+        "date_created": "03/30/2021",
+        "user_id": 1
     },
     {
         "title": "Authentication vs. Authorization",
-        "content": "There is a difference between authentication and authorization. Authentication is confirming your own identity, while authorization is being allowed access to something."
+        "content": "There is a difference between authentication and authorization. Authentication is confirming your own identity, while authorization is being allowed access to something.",
+        "date_created": "03/29/2021",
+        "user_id": 3
     },
     {
         "title": "What is an ORM?",
-        "content": "ORM stands for object relational mapper. It uses models to organize and access data. As a code-first model, you can write a database by writing the structure, then the ORM will create the tables and keys for you. It also abstracts database queries so that you don't have to write your own queries in a specific database language. It can really simplify the way you create queries in SQL!"
+        "content": "ORM stands for object relational mapper. It uses models to organize and access data. As a code-first model, you can write a database by writing the structure, then the ORM will create the tables and keys for you. It also abstracts database queries so that you don't have to write your own queries in a specific database language. It can really simplify the way you create queries in SQL!",
+        "date_created": "03/29/2021",
+        "user_id": 4
     }
 ]
 
