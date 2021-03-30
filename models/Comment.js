70ed287ed3d32e2feecfca261012abc3008f9495
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-30 12:05:07 -0700

    changed data types for content to TEXT

--- models/Comment.js
+++ models/Comment.js
@@ -12,7 +12,7 @@ Comment.init(
             autoIncrement: true,
         },
         content: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
         },
         date_created: {
             type: DataTypes.DATE,
