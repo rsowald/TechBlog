f05f3ed0cf1cc588b4cba09423e45e12f4c67a2b
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-29 19:17:22 -0700

    added some post seeds

---
+++ seeds/postData.js
@@ -0,0 +1,14 @@
+[
+    {
+        "title": "Why MVC is so important",
+        "content": "It's all about separation of concerns! You have the Model layer for data, the View layer for design, and the Controller layer for application logic. It is important to have separate code so each part has a meaningful and intuitive role while still being able adapt to change."
+    },
+    {
+        "title": "Authentication vs. Authorization",
+        "content": "There is a difference between authentication and authorization. Authentication is confirming your own identity, while authorization is being allowed access to something."
+    },
+    {
+        "title": "What is an ORM?",
+        "content": "ORM stands for object relational mapper. It uses models to organize and access data. As a code-first model, you can write a database by writing the structure, then the ORM will create the tables and keys for you. It also abstracts database queries so that you don't have to write your own queries in a specific database language. It can really simplify the way you create queries in SQL!"
+    }
+]
