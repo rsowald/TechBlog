7ba3261da0dd02cdf86c188c76e33e2f30405314
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-30 11:23:30 -0700

    made user seed data

---
+++ seeds/userData.js
@@ -0,0 +1,27 @@
+[
+    {
+        "name": "John Doe",
+        "email": "DoeRayMi@fakemail.com",
+        "password": "password1234"
+    },
+    {
+        "name": "Chris Jacobs",
+        "email": "CJacobs458@fakemail.com",
+        "password": "password1234"
+    },
+    {
+        "name": "Holly Benson",
+        "email": "ButterYellow123@fakemail.com",
+        "password": "password1234"
+    },
+    {
+        "name": "Yennifer Vangeberg",
+        "email": "BestMageEver@fakemail.com",
+        "password": "password1234"
+    },
+    {
+        "name": "Geralt Rivia",
+        "email": "Witcher999@fakemail.com",
+        "password": "password1234"
+    }
+]
