cc295340304b86931aad39df06ab2ef126bbe142
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-30 11:35:37 -0700

    modulized post seed data

--- seeds/postData.js
+++ seeds/postData.js
@@ -1,4 +1,6 @@
-[
+const { Post } = require('../models')
+
+const postData = [
     {
         "title": "Why MVC is so important",
         "content": "It's all about separation of concerns! You have the Model layer for data, the View layer for design, and the Controller layer for application logic. It is important to have separate code so each part has a meaningful and intuitive role while still being able adapt to change."
@@ -12,3 +14,7 @@
         "content": "ORM stands for object relational mapper. It uses models to organize and access data. As a code-first model, you can write a database by writing the structure, then the ORM will create the tables and keys for you. It also abstracts database queries so that you don't have to write your own queries in a specific database language. It can really simplify the way you create queries in SQL!"
     }
 ]
+
+const seedPost = () => Post.bulkCreate(postData);
+
+module.exports = seedPost;
