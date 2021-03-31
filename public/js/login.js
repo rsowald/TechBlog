0f69e11c9005aaebc2ad4ca39236eb2a90720ecf
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-30 22:49:04 -0700

    show only login or signup on login page

--- public/js/login.js
+++ public/js/login.js
@@ -1,55 +1,71 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
+$(document).ready(function () {
+    $('.signup-form').hide();
 
-    // Collect values from the login form
-    const email = document.querySelector('#login-email').value.trim();
-    const password = document.querySelector('#login-pass').value.trim();
 
-    if (email && password) {
-        // Send a POST request to the API endpoint
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+    const loginFormHandler = async (event) => {
+        event.preventDefault();
 
-        if (response.ok) {
-            // If successful, redirect the browser to the profile page
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
+        // Collect values from the login form
+        const email = document.querySelector('#login-email').value.trim();
+        const password = document.querySelector('#login-pass').value.trim();
+
+        if (email && password) {
+            // Send a POST request to the API endpoint
+            const response = await fetch('/api/users/login', {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (response.ok) {
+                // If successful, redirect the browser to the profile page
+                document.location.replace('/dashboard');
+            } else {
+                alert(response.statusText);
+            }
         }
-    }
-};
+    };
 
-const signupFormHandler = async (event) => {
-    event.preventDefault();
+    const signupFormHandler = async (event) => {
+        event.preventDefault();
 
-    const name = document.querySelector('#signup-name').value.trim();
-    const email = document.querySelector('#signup-email').value.trim();
-    const password = document.querySelector('#signup-pass').value.trim();
+        const name = document.querySelector('#signup-name').value.trim();
+        const email = document.querySelector('#signup-email').value.trim();
+        const password = document.querySelector('#signup-pass').value.trim();
 
-    if (name && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({ name, email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+        if (name && email && password) {
+            const response = await fetch('/api/users', {
+                method: 'POST',
+                body: JSON.stringify({ name, email, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
+            if (response.ok) {
+                document.location.replace('/dashboard');
+            } else {
+                alert(response.statusText);
+            }
         } else {
-            alert(response.statusText);
+            alert('You must enter a name, email, and password to continue.')
         }
-    } else {
-        alert('You must enter a name, email, and password to continue.')
-    }
-};
+    };
 
-document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+    document
+        .querySelector('.login-form')
+        .addEventListener('submit', loginFormHandler);
 
-document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+
+    document
+        .querySelector('.signup-form')
+        .addEventListener('submit', signupFormHandler);
+
+    $('.create').click(function () {
+        $('.login-form').hide();
+        $('.signup-form').show();
+    });
+
+    $('.login').click(function () {
+        $('.signup-form').hide();
+        $('.login-form').show();
+    });
+});
