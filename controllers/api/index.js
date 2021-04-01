ae6df73c014a0e872d037e00be083708daf6ab71
rsowald <72467920+rsowald@users.noreply.github.com>
2021-04-01 01:19:09 -0700

    Added comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const commentRoutes = require('./commentRoutes');
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
