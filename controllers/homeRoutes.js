4f4bdc14e9bfc4b95bf5e78b7e80b929eed47079
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-31 22:56:31 -0700

    added get route to dashboard for logged in user's posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,6 +27,27 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        console.log(user)
+
+        res.render('dashboard', {
+            user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect to dashboard
     if (req.session.logged_in) {
