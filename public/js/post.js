fe103a2135a822fdd631774a73814b4527d8cee6
rsowald <72467920+rsowald@users.noreply.github.com>
2021-04-01 01:19:30 -0700

    Comments are now saving and showing

--- public/js/post.js
+++ public/js/post.js
@@ -2,15 +2,15 @@
 const newCommentFormHandler = async (event) => {
     event.preventDefault();
 
-    const comment = document.querySelector('#comment').value.trim();
-    const id = event.currentTarget.getAttribute('post-id');
+    const content = document.querySelector('#comment').value.trim();
+    const post_id = event.currentTarget.getAttribute('post-id');
 
     const response = await fetch(`/api/comments`, {
         method: 'POST',
-        body: JSON.stringify({ id, comment }),
+        body: JSON.stringify({ post_id, content }),
         headers: {
             'Content-Type': 'application/json',
-        },
+        }
     });
 
     if (response.ok) {
